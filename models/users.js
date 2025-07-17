46b4c4dc5cefae2b454573969af943c6c4d1ec1f
Hazim Bakar <hazimbaa@gmail.com>
2025-07-17 14:42:29 +0800

    update db field courses and users

--- models/users.js
+++ models/users.js
@@ -1,36 +1,36 @@
 import mongoose from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
-const usersSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: uuidv4,
+const usersSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuidv4,
+    },
+    role: {
+      type: String,
+      enum: ["Instructor", "Student"],
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Inactive"],
+      default: "Active",
+    },
+    deleted_at: {
+      type: Date,
+      default: null,
+    },
   },
-  role: {
-    type: String,
-    enum: ["Instructor", "Student"],
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["Active", "Inactive"],
-    default: "Active",
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now,
-  },
-  deleted_at: {
-    type: Date,
-    default: null,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 export default mongoose.model("User", usersSchema);
