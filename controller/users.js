9ac32e7429b16cc586c1dfbf9ce452dc9ebe9a61
Hazim Bakar <hazimbaa@gmail.com>
2025-07-17 22:18:49 +0800

    add crud users

--- controller/users.js
+++ controller/users.js
@@ -2,7 +2,7 @@ import users from "../models/users.js";
 
 export const getUsers = async (req, res) => {
   try {
-    const usersList = await users.find({ deleted_at: null });
+    const usersList = await users.find();
 
     if (!usersList || usersList.length === 0) {
       return res.status(404).json({ message: "No users found" });
@@ -44,6 +44,72 @@ export const createUser = async (req, res) => {
       .json({ message: "User created successfully", user: newUser });
   } catch (error) {
     console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await users.findByIdAndUpdate(
+      id,
+      { status: "Inactive", deleted_at: Date.now() },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    res.status(200).json({ message: "User soft deleted successfully" });
+  } catch (error) {
+    console.error("Error soft deleting user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const restoreUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await users.findByIdAndUpdate(
+      id,
+      { status: "Active", deleted_at: null },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User restored successfully", user });
+  } catch (error) {
+    console.error("Error restoring user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  if (!updates || Object.keys(updates).length === 0) {
+    return res.status(400).json({ message: "No updates provided" });
+  }
+
+  try {
+    const user = await users.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+      updated_at: Date.now(),
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
+    console.error("Error updating user:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
